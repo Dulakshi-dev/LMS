79ae588e6dc0595e4180862349f64e15676cea81
kajanthann <148801608+kajanthann@users.noreply.github.com>
2025-01-04 12:43:18 +0530

    header, footer, main,openning hours pages are corrected

--- app/views/home/footer.view.php
+++ app/views/home/footer.view.php
@@ -1,23 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="index.css" />
-<footer class="bg-dark border-bottom border-body m-0">   
-    <div class="footer-content row">
-            <p class="col-lg-3 d-none d-lg-block" style="font-size:15px;">© Designed & Developed by Faculty Of Computing</p>
-            <div class="top-button col-12 col-lg-5 ">
-                <div class="footer-container">
-                    <a href="#top">TOP</a>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <title>Footer</title>
+</head>
+
+<body>
+    <footer class="bg-dark text-light m-0 py-3">
+        <div class="container">
+            <div class="row align-items-center d-flex justify-content-between">
+                <p class="col-lg-3 d-none d-lg-block mb-0" style="font-size: 13px;">
+                    © Designed & Developed by Faculty of Computing
+                </p>
+
+                <div class="border col-lg-3 d-none d-lg-block mb-0"></div>
+                <div class="border col-lg-3 d-none d-lg-block mb-0"></div>
+
+                <div class="col-12 col-lg-3 text-center text-lg-end">
+                    
+                <a href="#" class="btn btn-outline-light me-2">Back to Top</a>
+                <a href="#" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                <a href="#" class="text-white ms-3"><i class="fab fa-twitter fa-lg"></i></a>
+                <a href="#" class="text-white ms-3"><i class="fab fa-instagram fa-lg"></i></a>
+        
                 </div>
+
+                <p class="col-12 d-lg-none text-center mb-0" style="font-size: 15px;">
+                    © Designed & Developed by Faculty of Computing
+                </p>
             </div>
-            <div class="social-icons col-12 col-lg-3">
-                <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
-                <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a>
-            </div>
-            <p class="col-12 d-lg-none" style="font-size:15px;">© Designed & Developed by Faculty Of Computing</p>
-    </div>
-</footer>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>

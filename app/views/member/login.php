d4fd2e63541883c3e278187f25ad067a6a5071e4
kajanthann <arulkajanthan904@gmail.com>
2025-06-17 10:21:45 +0530

    member login done

--- app/views/member/login.php
+++ app/views/member/login.php
@@ -9,7 +9,6 @@ if (isset($_SESSION['member'])) {
     header("Location: index.php?action=dashboard");
     exit;
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,62 +19,63 @@ $pageCss = "member-login.css";
 require_once Config::getViewPath("common", "head.php");
 ?>
 
-<body>
-    <div class="container-fluid">
-        <div class="row mt-5 mb-5">
+<body class="d-flex flex-column min-vh-100 bg-dark">
+
+    <!-- Main content container -->
+    <div class="container-fluid flex-grow-1 d-flex align-items-center justify-content-center">
+        <div class="row w-100 mt-5 mb-5">
             <div class="col-12 col-lg-3 d-flex align-items-center justify-content-center">
                 <h2 class="text-white fs-1">Hi! Welcome Back</h2>
             </div>
             <div class="col-12 col-lg-6 pt-5">
-
                 <div class="text-white login-form col-12 col-lg-8 offset-lg-4">
                     <h1 class="text-center">Log In</h1>
 
                     <?php
                     $memberid = "";
-
                     if (isset($_COOKIE["memberid"])) {
                         $memberid = $_COOKIE["memberid"];
                     }
                     ?>
+
                     <form id="loginForm">
-                    <div class="form-group">
-                        <label for="memberid">Member ID</label>
-                        <input class="form-control mt-2" placeholder="M-XXXXXX" type="text" name="memid" id="memberid" value="<?php echo htmlspecialchars($memberid); ?>" >
-                        <span class="error text-danger" id="memberidError"></span>
-                    </div>
+                        <div class="form-group">
+                            <label for="memberid">Member ID</label>
+                            <input class="form-control mt-2" placeholder="M-XXXXXX" type="text" name="memid" id="memberid" value="<?php echo htmlspecialchars($memberid); ?>">
+                            <span class="error text-danger" id="memberidError"></span>
+                        </div>
 
-                    <div class="form-group mt-3">
-                        <label for="memberpw">Password</label>
-                        <div class="input-group">
-                            <input class="form-control " placeholder="Enter your password" type="password" name="memberpw" id="memberpw" >
-                            <span class="input-group-text" id="passwordToggle" style="cursor: pointer;">
-                                <i class="fas fa-eye" id="passwordIcon"></i>
-                            </span>
+                        <div class="form-group mt-3">
+                            <label for="memberpw">Password</label>
+                            <div class="input-group">
+                                <input class="form-control" placeholder="Enter your password" type="password" name="memberpw" id="memberpw">
+                                <span class="input-group-text" id="passwordToggle" style="cursor: pointer;">
+                                    <i class="fas fa-eye" id="passwordIcon"></i>
+                                </span>
+                            </div>
+                            <span class="error text-danger" id="passwordError"></span>
                         </div>
-                        <span class="error text-danger" id="passwordError"></span>
-                    </div>
 
-                    <div class="row mt-2">
-                        <div class="col">
-                            <input type="checkbox" name="rememberme" id="rememberme">
-                            <label for="rememberme">Remember me</label>
+                        <div class="row mt-2">
+                            <div class="col">
+                                <input type="checkbox" name="rememberme" id="rememberme">
+                                <label for="rememberme">Remember me</label>
+                            </div>
+                            <div class="col d-flex justify-content-end">
+                                <a href="<?php echo Config::indexPathMember() ?>?action=showforgotpw" class="text-decoration-none forgot-password">Forgot Password?</a>
+                            </div>
                         </div>
-                        <div class="col d-flex justify-content-end">
-                            <a href="<?php echo Config::indexPathMember() ?>?action=showforgotpw" class="text-decoration-none forgot-password">Forgot Password?</a>
-                        </div>
-                    </div>
 
-                    <button type="submit" class="btn btn-primary w-100 my-4">Login</button>
+                        <button type="submit" class="btn btn-primary w-100 my-4">Login</button>
 
-                    <div class="row">
-                        <div class=" col-md-6">
-                            <p>Not registered yet?</p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Not registered yet?</p>
+                            </div>
+                            <div class="col-md-6 d-flex justify-content-end">
+                                <a href="<?php echo Config::indexPathMember() ?>?action=register" class="text-decoration-none create-account">Create an account</a>
+                            </div>
                         </div>
-                        <div class="col-md-6 d-flex justify-content-end">
-                            <a href="<?php echo Config::indexPathMember() ?>?action=register" class="text-decoration-none create-account">Create an account</a>
-                        </div>
-                    </div>
                     </form>
                 </div>
             </div>
@@ -83,16 +83,16 @@ require_once Config::getViewPath("common", "head.php");
     </div>
     <?php require_once Config::getViewPath("common", "footer.php"); ?>
 
-
     <script>
-        // Handle form submit with Enter or button
+        // Handle form submit
         document.getElementById('loginForm').addEventListener('submit', function (event) {
             event.preventDefault();
             login();
         });
 
-        document.getElementById('passwordToggle').addEventListener('click', function() {
-            var passwordField = document.getElementById('password');
+        // Toggle password visibility
+        document.getElementById('passwordToggle').addEventListener('click', function () {
+            var passwordField = document.getElementById('memberpw');
             var passwordIcon = document.getElementById('passwordIcon');
 
             if (passwordField.type === "password") {
@@ -106,11 +106,10 @@ require_once Config::getViewPath("common", "head.php");
             }
         });
     </script>
+
     <script src="<?php echo Config::getJsPath("memberLogin.js"); ?>"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
